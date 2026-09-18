f5745a8192cfc286211144f34522190ea2b64bc3
sap586 <sap586@nyu.edu>
2026-09-18 13:26:20 -0400

    Add files via upload

--- docs/app.js
+++ docs/app.js
@@ -1,149 +1,90 @@
-let masterData = null;
-const TAGS = ["software", "robotics", "architect", "management"];
+let masterData=null;
+const TAGS=["software","robotics","architect","management"];
 
-async function init() {
-    const response = await fetch("./data/master-resume.json");
-    masterData = await response.json();
-    renderFiltered();
+async function init(){
+ const r=await fetch('./data/master-resume.json');
+ masterData=await r.json();
+ render();
+ updateStats();
 }
 
-function updateStats() {
-    const bulletCount = masterData.experience.reduce(
-        (total, job) => total + job.bullets.length,
-        0,
-    );
-    document.getElementById("stats").textContent =
-        `Companies: ${masterData.experience.length} | Bullets: ${bulletCount}`;
+function updateStats(){
+ let bullets=0;
+ masterData.experience.forEach(j=>bullets+=j.bullets.length);
+ document.getElementById('stats').innerHTML=`Companies: ${masterData.experience.length} | Bullets: ${bullets}`;
 }
 
-function renderFiltered() {
-    const selectedTag = document.getElementById("resumeType").value;
-    const jobs = document.getElementById("jobs");
-    jobs.innerHTML = "";
+function render(){
+ const jobs=document.getElementById('jobs');
+ jobs.innerHTML='';
 
-    masterData.experience.forEach((job, jobIndex) => {
-        const visibleBullets = job.bullets.filter((bullet) =>
-            bullet.tags.includes(selectedTag),
-        );
-        const jobElement = document.createElement("div");
-        jobElement.className = "job";
-        jobElement.innerHTML = `
-            <input type="text" value="${escapeHtml(job.company)}" onchange="updateCompany(${jobIndex}, this.value)">
-            <input type="text" value="${escapeHtml(job.title)}" onchange="updateTitle(${jobIndex}, this.value)">
-            <button onclick="deleteCompany(${jobIndex})">Delete Company</button>
-        `;
+ masterData.experience.forEach((job,ji)=>{
+  const div=document.createElement('div');
+  div.className='job';
 
-        visibleBullets.forEach((bullet) => {
-            const bulletIndex = job.bullets.indexOf(bullet);
-            const bulletElement = document.createElement("div");
-            bulletElement.className = "bullet";
-            bulletElement.innerHTML = `
-                <input type="text" value="${escapeHtml(bullet.text)}" onchange="updateBullet(${jobIndex}, ${bulletIndex}, this.value)">
-                <div class="tags">${TAGS.map((tag) => `
-                    <label><input type="checkbox" ${bullet.tags.includes(tag) ? "checked" : ""}
-                        onchange="toggleTag(${jobIndex}, ${bulletIndex}, '${tag}', this.checked)">${tag}</label>
-                `).join("")}</div>
-                <button onclick="deleteBullet(${jobIndex}, ${bulletIndex})">Delete Bullet</button>
-            `;
-            jobElement.appendChild(bulletElement);
-        });
+  let html=`<input type=text value="${job.company}" onchange="masterData.experience[${ji}].company=this.value"><br><input type=text value="${job.title}" onchange="masterData.experience[${ji}].title=this.value"><button onclick="deleteCompany(${ji})">Delete Company</button>`;
 
-        jobElement.insertAdjacentHTML(
-            "beforeend",
-            `<button onclick="addBullet(${jobIndex})">Add Bullet</button>`,
-        );
-        jobs.appendChild(jobElement);
-    });
-    updateStats();
+  job.bullets.forEach((b,bi)=>{
+   html+=`<div class='bullet'><input type=text value="${b.text}" onchange="masterData.experience[${ji}].bullets[${bi}].text=this.value"><div class='tags'>`+
+   TAGS.map(t=>`<label><input type='checkbox' ${b.tags.includes(t)?'checked':''} onchange='toggleTag(${ji},${bi},"${t}",this.checked)'>${t}</label>`).join('')+
+   `</div><button onclick='deleteBullet(${ji},${bi})'>Delete Bullet</button></div>`;
+  });
+
+  html+=`<button onclick='addBullet(${ji})'>Add Bullet</button>`;
+  div.innerHTML=html;
+  jobs.appendChild(div);
+ });
+
+ updateStats();
 }
 
-function escapeHtml(value) {
-    return String(value)
-        .replaceAll("&", "&amp;")
-        .replaceAll("<", "&lt;")
-        .replaceAll(">", "&gt;")
-        .replaceAll('"', "&quot;")
-        .replaceAll("'", "&#039;");
+function toggleTag(j,b,t,c){
+ let bullet=masterData.experience[j].bullets[b];
+ if(c){
+  if(!bullet.tags.includes(t)) bullet.tags.push(t);
+ } else {
+  bullet.tags=bullet.tags.filter(x=>x!==t);
+ }
+ render();
 }
 
-function updateCompany(index, value) {
-    masterData.experience[index].company = value;
+function addBullet(j){
+ masterData.experience[j].bullets.push({text:'New Bullet',tags:[]});
+ render();
 }
 
-function updateTitle(index, value) {
-    masterData.experience[index].title = value;
+function deleteBullet(j,b){
+ masterData.experience[j].bullets.splice(b,1);
+ render();
 }
 
-function updateBullet(jobIndex, bulletIndex, value) {
-    masterData.experience[jobIndex].bullets[bulletIndex].text = value;
+function addCompany(){
+ masterData.experience.push({company:'New Company',title:'New Position',bullets:[]});
+ render();
 }
 
-function toggleTag(jobIndex, bulletIndex, tag, checked) {
-    const tags = masterData.experience[jobIndex].bullets[bulletIndex].tags;
-    if (checked && !tags.includes(tag)) tags.push(tag);
-    if (!checked) {
-        masterData.experience[jobIndex].bullets[bulletIndex].tags =
-            tags.filter((item) => item !== tag);
-    }
-    renderFiltered();
+function deleteCompany(i){
+ masterData.experience.splice(i,1);
+ render();
 }
 
-function addBullet(jobIndex) {
-    masterData.experience[jobIndex].bullets.push({
-        text: "New Bullet",
-        tags: [document.getElementById("resumeType").value],
-    });
-    renderFiltered();
+function downloadFile(obj,name){
+ const a=document.createElement('a');
+ a.href=URL.createObjectURL(new Blob([JSON.stringify(obj,null,2)],{type:'application/json'}));
+ a.download=name;
+ a.click();
 }
 
-function deleteBullet(jobIndex, bulletIndex) {
-    masterData.experience[jobIndex].bullets.splice(bulletIndex, 1);
-    renderFiltered();
-}
+function downloadMasterJson(){downloadFile(masterData,'master-resume.json');}
 
-function addCompany() {
-    masterData.experience.push({
-        company: "New Company",
-        title: "New Position",
-        bullets: [],
-    });
-    renderFiltered();
-}
-
-function deleteCompany(index) {
-    masterData.experience.splice(index, 1);
-    renderFiltered();
-}
-
-function downloadFile(object, filename) {
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(
-        new Blob([JSON.stringify(object, null, 2)], { type: "application/json" }),
-    );
-    link.download = filename;
-    link.click();
-    URL.revokeObjectURL(link.href);
-}
-
-function downloadMasterJson() {
-    downloadFile(masterData, "master-resume.json");
-}
-
-function downloadFilteredJson() {
-    const selectedTag = document.getElementById("resumeType").value;
-    const filteredResume = {
-        profile: masterData.profile,
-        resumeType: selectedTag,
-        experience: masterData.experience
-            .map((job) => ({
-                ...job,
-                bullets: job.bullets.filter((bullet) =>
-                    bullet.tags.includes(selectedTag),
-                ),
-            }))
-            .filter((job) => job.bullets.length > 0),
-    };
-    downloadFile(filteredResume, `${selectedTag}-resume.json`);
+function downloadFilteredJson(){
+ const tag=document.getElementById('resumeType').value;
+ const out={profile:masterData.profile,resumeType:tag,experience:[]};
+ masterData.experience.forEach(j=>{
+  const bullets=j.bullets.filter(b=>b.tags.includes(tag));
+  if(bullets.length) out.experience.push({...j,bullets});
+ });
+ downloadFile(out,tag+'-resume.json');
 }
 
 init();
